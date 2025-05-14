0f4a9da3618bf95f5578921b36339e634777f88b
navami21 <navamib2023@gmail.com>
2025-05-14 20:58:04 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ const Home = () => {
   return (
   <div className="home-container">
     <div className="slideshow-container">
-    <div className="slide fade"><img src="/src/images/img7.jpeg" alt="Slide 1" /></div>
-    <div className="slide fade"><img src="/src/images/img5.jpg" alt="Slide 2" /></div>
-    <div className="slide fade"><img src="/src/images/img6.jpg" alt="Slide 3" /></div>
-    <div className="slide fade"><img src="/src/images/img10.jpg" alt="Slide 4" /></div>
+    <div className="slide fade"><img src="/images/img1.jpg" alt="Slide 1" /></div>
+    <div className="slide fade"><img src="/images/img5.jpg" alt="Slide 2" /></div>
+    <div className="slide fade"><img src="/images/img6.jpg" alt="Slide 3" /></div>
+    <div className="slide fade"><img src="/images/img10.jpg" alt="Slide 4" /></div>
   </div>
 
   <div className="container text-center mt-4">
